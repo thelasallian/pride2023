6923c9284da26dd8cf683017ecb95ff13e8c1b16
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-05-30 12:33:49 +0800

    Basic HTML boilerplate

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Webpage Title -->
+    <!-- TODO: Replace with final special name -->
+    <title>Pride Special Title - The LaSallian</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
