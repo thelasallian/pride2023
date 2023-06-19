54c8d27651c52d11b1e084dbcbb3c6a7f051315c
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-06-19 12:36:35 +0800

    Removed lorem ipsum

--- php/modules/homepage/legislation.php
+++
@@ -1,1 +0,0 @@
-Curabitur blandit tempus ardua ridiculus sed magna. Sed haec quis possit intrepidus aestimare tellus. Quisque ut dolor gravida, placerat libero vel, euismod. Plura mihi bona sunt, inclinet, amari petere vellent.
